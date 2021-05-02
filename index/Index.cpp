72d61a411cdae39969ef1f283d77d24706c6346c
Valemos <programworkerbox@gmail.com>
2021-05-02 19:00:28 +0300

    creating simple index. added token transformation

--- index/Index.cpp
+++ index/Index.cpp
@@ -1,11 +1,11 @@
 #include "Index.h"
 
-#include <filesystem>
-#include <fstream>
 #include <boost/log/trivial.hpp>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
 
 namespace fs = std::filesystem;
-
 
 
 Index::Index(size_t total_files) 
@@ -15,29 +15,71 @@ Index::Index(size_t total_files)
 
 void Index::createFromDirectory(std::string directory_path) 
 {
-    for (auto& entry : fs::recursive_directory_iterator(directory_path)){
+    for (auto& entry : fs::recursive_directory_iterator(directory_path)) {
         if (entry.is_regular_file()) {
-            addFileIndex(entry.path().string());
+            addFile(entry.path().string());
+            break;
         }
     }
 }
 
-void Index::addFileIndex(std::string path) 
+void Index::addFile(std::string path) 
 {
     BOOST_LOG_TRIVIAL(info) << path;
 
     // path_id is index of document_paths_ vector
-    int path_id = document_paths_.size();
+    int document_id = (int) document_paths_.size();
     document_paths_.push_back(path);
 
-    Index::TermPosition current_position;
-
     std::ifstream fin(path);
-    if (!fin.bad()){
+    if (!fin.bad()) {
 
-        // todo iterate all words and add them to index
+        std::streampos word_start = 0;
+        std::string word;
+        while(!fin.eof()) {
+
+            word_start = fin.tellg();
+            fin >> word;
+
+            addTerm(normalizeToken(word), {document_id, word_start});
+        }
 
     } else {
         BOOST_LOG_TRIVIAL(error) << "cannot read file " << path;
     }
+}
+
+void Index::addTerm(const std::string& word, TermPosition position) 
+{
+    auto result = term_positions_.find(word);
+    if (result != term_positions_.end()){
+        result->second.emplace_back(position);
+    } else {
+        std::list<TermPosition> positions_new;
+        positions_new.emplace_back(position);
+
+        term_positions_.emplace(word, std::move(positions_new));
+    }
+}
+
+std::string Index::normalizeToken(const std::string& word) 
+{
+    // preallocate memory for new token
+    std::string token;
+    token.reserve(word.size());
+    std::stringstream ss(std::move(token));
+
+    for (auto& letter : word) {
+        if (letter != '.' && 
+            letter != ',' && 
+            letter != '?' && 
+            letter != '!' && 
+            letter != '(' && 
+            letter != ')')
+        {
+            ss << std::tolower(letter, std::locale());
+        }
+    }
+
+    return ss.str();
 }
--- index/Index.h
+++ index/Index.h
@@ -5,6 +5,7 @@
 #include <list>
 #include <map>
 #include <vector>
+#include <fstream>
 
 
 class Index {
@@ -14,22 +15,22 @@ public:
     Index(size_t total_files);
 
     void createFromDirectory(std::string directory_path);
-    void addFileIndex(std::string path);
+    void addFile(std::string path);
 
-    void saveToFile(std::string path);
-    void loadFromFile(std::string path);
+    void save(std::string path);
+    void load(std::string path);
 
 private:
     struct TermPosition {
         int document_index;
-        long word_index;
+        std::streampos word_position;
     };
 
     std::vector<std::string> document_paths_;
     std::map<std::string, std::list<TermPosition>> term_positions_;
 
-
-
+    void addTerm(const std::string& word, TermPosition position);
+    std::string normalizeToken(const std::string& word);
 };
 
 #endif // __INDEX_H__
